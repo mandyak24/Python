1184a65194628cab1dcf05a80eb98c580ddba4d8
Amanda <amandaroblesurena@gmail.com>
2022-04-08 16:58:37 +0200

    first commit

---
+++ ejercicio4.1Python.py
@@ -0,0 +1,6 @@
+pregunta=int(input("Que edad tienes?"))
+
+if pregunta <18:
+    print("El usuario es menor de edad")
+else: 
+    print("El usuario es mayor de edad")
---
+++ switch.py
@@ -0,0 +1,36 @@
+valor =3
+#palabra nueva en Python : match (es como switch)
+match valor:
+    case 1:
+        print("Valor es 1")
+    case 2:
+        print("Valor es 2")
+    case 3:
+        print("Valor es 3")
+    case 4:
+        print("Valor es 4")
+    case 5:
+        print("Valor es 5")
+
+
+#Else del for- se ejecuta siempre que el for no se haya roto (con break)
+lista=["hola", "mensaje","adios"]
+
+for palabra in lista:
+    if palabra=="mensaje";
+        print("Encuentro la palabra mensaje")
+        break
+else:
+    print("No encuentra")
+
+#palabra reservada pass- cuando quieres escribir algo pero aun no lo tienes claro
+#Tenemos una lista de palabras y vamos a buscar la mas cercana con el algoritmo de XXXX
+#
+#TODO: Implementar el bucle
+for palabra in lista:
+    pass
+
+if 5<6:
+    pass
+while False:
+    pass
