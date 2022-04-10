e5ba0107aaf2219f9f74ccffe6c5c4ebfa492ae7
Amanda <amandaroblesurena@gmail.com>
2022-04-10 21:47:52 +0200

    6.1

---
+++ Ejercicio6.1Python.py
@@ -0,0 +1,24 @@
+#Ejercicio 1
+
+class Vehiculo():
+    color=""
+    ruedas=0
+    puertas=0
+
+    def __init__(self,color,ruedas,puertas):
+        self.color=color
+        self.ruedas=ruedas
+        self.puertas=puertas
+
+class Coche(Vehiculo):
+
+    velocidad=0
+    cilindrada=0
+
+    def ___init__(self,velocidad,cilindrada):
+      self.velocidad=velocidad
+      self.cilindrada=cilindrada
+
+
+
+coche=Coche("Azul",4,5)
