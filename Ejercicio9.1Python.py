68832c8f7d3a59f56fd58d378729dd1017f04d97
Amanda <amandaroblesurena@gmail.com>
2022-04-17 20:22:02 +0200

    Modified9.1

--- Ejercicio9.1Python.py
+++ Ejercicio9.1Python.py
@@ -1,5 +1,5 @@
 
-#9.1.Crea un script que le pida al usuario una lista de países (separados por comas). Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set).
+#9.1..Crea un script que le pida al usuario una lista de países (separados por comas). Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set).
 # Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados por comas.
 
 paises=[]
