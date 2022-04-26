a025887e3ee5e64b7f942c4c016d1237cd294eb1
Amanda <amandaroblesurena@gmail.com>
2022-04-26 18:43:54 +0200

    10.1

---
+++ Ejercicio10.1Python.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+def selec():
+    monitor.config(text = "Opción {}".format(opcion.get() ) )
+
+root = Tk()
+root.config(bd=15)
+
+opcion = IntVar() # Como StrinVar pero en entero
+
+Radiobutton(root, text="Opción 1", variable=opcion, 
+            value=1, command=selec).pack()
+Radiobutton(root, text="Opción 2", variable=opcion,
+            value=2, command=selec).pack()
+Radiobutton(root, text="Opción 3", variable=opcion, 
+            value=3, command=selec).pack()
+
+monitor = Label(root)
+monitor.pack()
+
+root.mainloop()
+
+def reset():
+    opcion.set(None)          # Reiniciamos el seleccionable
+    monitor.config(text='')   # Reiniciamos la etiqueta
+
+Button(root, text="Reiniciar", command=reset).pack()
